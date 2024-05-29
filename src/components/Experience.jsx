c75f41ca8b3ee6e4792122512b630f6da32a70a0
CodeBy-Avinash <eavinash077@gmail.com>
2024-05-29 08:22:49 +0530

    experience update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,7 +3,7 @@ import { EXPERIENCES } from "../constants";
 const Experience = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-        <h1 className="my-20 text-center text-4xl">Experience</h1>
+        <h1 className="my-20 text-center text-4xl">Experiencess</h1>
     <div>
     {EXPERIENCES.map((experience,index) => (
         <div 
