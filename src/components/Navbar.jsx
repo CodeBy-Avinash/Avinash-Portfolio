969fb3331ba949668c14ed877a82737d6865f86f
CodeBy-Avinash <eavinash077@gmail.com>
2024-05-29 12:57:06 +0530

    "removed twitter"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import logo from "../assets/kevinRushLogo.png";
 import {FaLinkedin} from "react-icons/fa";
 import {FaGithub} from "react-icons/fa";
-import {FaSquareXTwitter} from "react-icons/fa6";
 import {FaInstagram} from "react-icons/fa";
 const Navbar = () => {
   return (
@@ -14,7 +13,6 @@ const Navbar = () => {
             <FaLinkedin />
             <FaGithub />
             <FaInstagram />
-            <FaSquareXTwitter />
         </div>
     </nav>
   )
