f56a965ad2e9722efeb757a57efb0e1a7905775c
CodeBy-Avinash <eavinash077@gmail.com>
2024-05-29 14:33:31 +0530

    changed img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,37 @@
-import logo from "../assets/kevinRushLogo.png";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import aboutImg from "../assets/about.jpg";
+import { ABOUT_TEXT } from "../constants";
+import { motion } from "framer-motion";
 
-const Navbar = () => {
+const About = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 items-center">
-        {/* <img className="mx-2 w-10" src={logo} alt="logo" /> */}
-        <h1 className="text-4xl bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text tracking-tight text-transparent">Avinash Portfolio</h1>
+    <div id="about" className="border-b border-neutral-900 pb-4">
+      <h1 className="my-20 text-center text-4xl">
+        About <span className="text-neutral-500">Me</span>
+      </h1>
+      <div className="flex flex-wrap">
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8"
+        >
+          <div className="flex items-center justify-center">
+            <img className="rounded-2xl w-3/4 lg:w-1/2" src={aboutImg} alt="about" />
+          </div>
+        </motion.div>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 flex items-center justify-center lg:justify-start"
+        >
+          <div className="my-4 max-w-xl py-6">
+            <p>{ABOUT_TEXT}</p>
+          </div>
+        </motion.div>
       </div>
-      <div className="flex items-center justify-center gap-4 text-xl">
-        <a href="#about" className="hover:text-pink-300">About</a>
-        <a href="#projects" className="hover:text-pink-300">Projects</a>
-        <a href="#contact" className="hover:text-pink-300">Contact</a>
-      </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-        <a href="https://www.github.com" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-      </div>
-    </nav>
+    </div>
   );
-};
+}
 
-export default Navbar;
+export default About;
